bb49b828e1cd76ca7da2c0c260eb3879b51f7ebb
quackable <manalog44@gmail.com>
2021-06-03 17:31:10 +0300

    Moved from O(NM) to O(N + M) using prefix func

--- main.cpp
+++ main.cpp
@@ -17,32 +17,43 @@ struct TimeMeasurer {
   ~TimeMeasurer() {
     auto now = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = now - start;
-    std::cout << "Duration of \"" << description << "\" is ";
+    std::cout << "Duration of \"" << description << "\" was ";
     std::cout << std::setprecision(8) << duration.count() << "s\n";
   }
   std::string description;
   std::chrono::time_point<std::chrono::high_resolution_clock> start;
 };
 
-void FindEntries(const char* pi,
-                 size_t from,
-                 size_t to,
+void FindEntries(char const* text,
+                 size_t length,
                  const std::string& string,
                  std::vector<size_t>& results,
                  std::mutex& mutex,
                  size_t thread_number = 0) {
   {
     TimeMeasurer T("Thread #" + std::to_string(thread_number));
-    for (size_t i = from; i < to; i++) {
-      if (i + string.size() <= to
-          && std::string(pi + i, pi + i + string.size()) == string) {
-        std::lock_guard<std::mutex> lg(mutex);
-        results.push_back(i);
+    std::vector<int> prefix_function(length);
+    std::string full_text = string + "#" + std::string(text, text + length);
+    for (int i = 1; i < prefix_function.size(); i++) {
+      int j = prefix_function[i - 1];
+      while (j > 0 && full_text[i] != full_text[j]) {
+        j = prefix_function[j - 1];
+      }
+      if (full_text[i] == full_text[j]) {
+        j++;
+      }
+      prefix_function[i] = j;
+    }
+    std::vector<size_t> res;
+    for (int i = 0; i < prefix_function.size(); i++) {
+      if (prefix_function[i] == string.size()) {
+        res.push_back(i - 2 * string.size());
       }
     }
     mutex.lock();
+    results.insert(results.end(), res.begin(), res.end());
+    mutex.unlock();
   }
-  mutex.unlock();
 }
 
 std::vector<std::pair<size_t, size_t>> GenerateBlocks(size_t from,
@@ -54,12 +65,13 @@ std::vector<std::pair<size_t, size_t>> GenerateBlocks(size_t from,
   assert(word_size > 0);
   std::vector<std::pair<size_t, size_t>> answer;
   answer.reserve(block_count);
-  size_t recommended_block_size = (to - from) / block_count;
+  size_t target_block_size = (to - from) / block_count;
   size_t last_index{0};
   for (int i = 0; i < block_count - 1; i++) {
-    answer.emplace_back(last_index,
-                        last_index + recommended_block_size + word_size - 1);
-    last_index += recommended_block_size;
+    answer.emplace_back(
+        last_index,
+        std::min(to, last_index + target_block_size + word_size - 1));
+    last_index += target_block_size;
   }
   answer.emplace_back(last_index, to);
   for (size_t i = 0; i < answer.size(); i++) {
@@ -70,47 +82,60 @@ std::vector<std::pair<size_t, size_t>> GenerateBlocks(size_t from,
   return answer;
 }
 
+std::string GetGreenText(const std::string& string) {
+  return "\033[1;32m" + string + "\033[0m";
+}
+
+std::string GetRedText(const std::string& string) {
+  return "\033[31m" + string + "\033[0m";
+}
+
+std::string GetBlueText(const std::string& string) {
+  return "\033[34m" + string + "\033[0m";
+}
+
 int main() {
   std::string filename;
-  std::cout << "Enter filename (or \"0\" for pi.txt)\n";
+  std::cout << GetGreenText("Enter filename (or \"0\" for pi.txt)\n");
   std::cin >> filename;
   if (filename == "0") {
     filename = "pi.txt";
   }
   std::ifstream input_file("../" + filename);
   if (!input_file.is_open()) {
-    std::cout << "Invalid filename or unreachable file";
+    std::cout << GetRedText("Invalid filename or unreachable file " + filename);
     return 1;
   }
   std::cout << std::fixed << std::setprecision(2);
 
-  int max_size = 1e9;
-  char* pi = new char[max_size + 1];
+  int size = 1e9;
+  char* text = new char[size + 1];
   {
     TimeMeasurer T("Loading");
     int i = 0;
-    while (!input_file.eof() && i < max_size) {
-      if (i % (max_size / 25) == 0) {
-        std::cout << "Loading... " << 100 * (double) i / max_size << "%"
-                  << '\n';
+    while (!input_file.eof() && i < size) {
+      if (i % (size / 25) == 0) {
+        std::cout
+            << "Loading... " << 100 * (double) i / size << "%" << '\n';
       }
-      pi[i++] = input_file.get();
+      text[i++] = input_file.get();
     }
-    max_size = i;
+    size = i;
     std::cout << "Loaded!\n";
   }
 
   while (true) {
-    std::cout << "====\nCommands:\nFind (integer: number to find) "
-                 "(integer: number of threads)\n"
-                 "Substr (integer: position) (integer: count)\n====\n";
+    std::cout << GetGreenText(
+        "====\nCommands:\nFind (integer: number to find) "
+        "(integer: number of threads)\n"
+        "Substr (integer: position) (integer: count)\n====\n");
 
     std::string request;
     std::cin >> request;
     if (request == "Find" || request == "find") {
-      std::string needed;
+      std::string string;
       int thread_number;
-      std::cin >> needed >> thread_number;
+      std::cin >> string >> thread_number;
 
       std::vector<size_t> indexes;
       std::mutex indexes_mutex;
@@ -118,14 +143,14 @@ int main() {
 
       {
         TimeMeasurer T("Search");
-        auto blocks = GenerateBlocks(0, max_size, needed.size(), thread_number);
+        auto blocks = GenerateBlocks(0, size, string.size(), thread_number);
         std::cout << "Running on " << blocks.size() << " threads\n";
         for (int i = 0; i < thread_number; i++) {
           threads[i] = std::make_unique<std::thread>(FindEntries,
-                                                     std::ref(pi),
-                                                     blocks[i].first,
-                                                     blocks[i].second,
-                                                     needed,
+                                                     text + blocks[i].first,
+                                                     blocks[i].second
+                                                         - blocks[i].first,
+                                                     string,
                                                      std::ref(indexes),
                                                      std::ref(indexes_mutex),
                                                      i);
@@ -134,8 +159,10 @@ int main() {
           thread->join();
         }
       }
-      std::cout << "--Found " << indexes.size() << " entries--\n";
-      std::cout << "How many entries show (they are not sorted)?\n";
+      std::cout << GetGreenText(
+          "--Found " + std::to_string(indexes.size()) + " entries--\n");
+      std::cout
+          << GetGreenText("How many entries show?\n");
       int number;
       std::cin >> number;
 
@@ -144,10 +171,11 @@ int main() {
         std::sort(indexes.begin(), indexes.end());
       }
 
-      std::cout << "Found entries from positions (1-indexation):\n";
+      std::cout
+          << GetBlueText("Found entries from positions (1-indexation):\n");
       for (int i = 0; i < std::min(static_cast<int>(indexes.size()), number);
            i++) {
-        std::cout << indexes[i] + 1 << '\n';
+        std::cout << GetBlueText(std::to_string(indexes[i] + 1)) << '\n';
       }
     } else {
       int pos;
@@ -155,7 +183,7 @@ int main() {
       std::cin >> pos >> cnt;
       pos--;
       for (int i = pos; i < pos + cnt; i++) {
-        std::cout << pi[i];
+        std::cout << text[i];
       }
       std::cout << '\n';
       break;
